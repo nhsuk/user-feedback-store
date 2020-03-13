9bbc1aa86f95af5723712d83d00ed5d72581b299
Mike Monteith <mike@mikemonteith.com>
2020-03-13 14:00:13 +0000

    Use more idiomatic return method

--- feedback/index.js
+++ feedback/index.js
@@ -1,17 +1,14 @@
-/* eslint-disable no-param-reassign */
-
-module.exports = async (context, req) => {
+module.exports = (context, req) => {
   context.log('JavaScript HTTP trigger function processed a request.');
 
   if (req.query.name || (req.body && req.body.name)) {
-    context.res = {
-      // status: 200, /* Defaults to 200 */
+    context.done(null, {
       body: `Hello ${req.query.name || req.body.name}`,
-    };
+    });
   } else {
-    context.res = {
+    context.done(null, {
       body: 'Please pass a name on the query string or in the request body',
       status: 400,
-    };
+    });
   }
 };
