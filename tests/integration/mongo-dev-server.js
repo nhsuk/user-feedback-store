ea4b7fde2d3b520f4a9fcf169f3680c67bd3d1f6
Mike Monteith <mike@mikemonteith.com>
2020-05-13 14:05:26 +0100

    Check the dev mongo server is using the right port
    By default, mongodb-memory-server will fall back to using a random port if the requested
    port is already being used. We want to avoid this for simplicity.
    
    * After starting the server, check the port is correct.
    * If not, stop with a warning.

--- tests/integration/mongo-dev-server.js
+++ tests/integration/mongo-dev-server.js
@@ -1,13 +1,25 @@
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
+const PORT = 51412;
+
 const mongod = new MongoMemoryServer({
   instance: {
     dbName: 'userfeedback',
-    port: 51412,
+    port: PORT,
   },
 });
 
-mongod.getUri().then((uri) => {
-  // eslint-disable-next-line no-console
-  console.info(`mongodb memory server available at ${uri}`);
+// Make sure that the correct port could be used and we haven't fallen-back to a random port number.
+mongod.getPort().then((port) => {
+  if (port !== PORT) {
+    mongod.stop();
+    // eslint-disable-next-line no-console
+    console.error(`Error: port ${PORT} is not available. Do you already have this server running?`);
+    process.exit(1);
+  }
+
+  mongod.getUri().then((uri) => {
+    // eslint-disable-next-line no-console
+    console.info(`mongodb memory server available at ${uri}`);
+  });
 });
