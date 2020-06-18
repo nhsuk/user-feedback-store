20ddb6c8368932b575fc3e3e5670662dbdf72aca
Mike Monteith <mike@mikemonteith.com>
2020-06-18 16:31:30 +0100

    Add utilities for extracting data to CSV

---
+++ bin/comments.js
@@ -0,0 +1,46 @@
+const { getAllComments } = require('../lib/database.js');
+
+const arrayToCSVRow = (array) => {
+  const values = array
+    .map((value) => String(value))
+    .map((value) => value.replace('"', '\\"')) // convert doublequote to escaped-doublequote
+    .map((value) => `"${value}"`) // Wrap values in doublequotes
+    .join(','); // Join array together with commas
+
+  // Add a newline to end the row
+  return `${values}\n`;
+};
+
+(async () => {
+  const data = await getAllComments();
+
+  const cleanData = data.map((row) => ({
+    ...row,
+    timestamp: row.timestamp.toISOString(),
+  }));
+
+  // Turn comments into CSV
+  const columns = [{
+    key: 'timestamp',
+    label: 'datetime',
+  }, {
+    key: 'url',
+    label: 'URL',
+  }, {
+    key: 'isSatisfied',
+    label: 'Satisfied',
+  }, {
+    key: 'comments',
+    label: 'Comments',
+  }];
+
+  // Write the header labels as the first row
+  const headerLabels = columns.map((column) => column.label);
+  process.stdout.write(arrayToCSVRow(headerLabels));
+
+  // Write each data row
+  cleanData.forEach((row) => {
+    const values = columns.map((column) => row[column.key]);
+    process.stdout.write(arrayToCSVRow(values));
+  });
+})();
