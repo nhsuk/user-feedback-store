bc95760a73108f3e06e90b5b6b25162ca08b77e7
Mike Monteith <mike@mikemonteith.com>
2020-07-13 09:23:11 +0100

    Add integration tests for the averages cli

--- tests/integration/cli.test.js
+++ tests/integration/cli.test.js
@@ -54,39 +54,45 @@ afterAll(async () => {
   await connection.close();
 });
 
+/**
+ * Run a CLI command, capture stdout, parse stdout into a CSV.
+ * @param {string} command - Command to pass to yargs.parse
+ *
+ * Returns array:
+ * 0: CSV parsing errors
+ * 1: array of arrays. (CSV rows by columns)
+ */
+const getCSVFromCommand = (command) => new Promise((resolve) => {
+  const stdout = mockStdout();
+  cli
+    .onFinishCommand(() => {
+      const stdOutString = stdout.read();
+      stdout.restore();
+      csvParse(stdOutString, (err, output) => {
+        resolve([err, output]);
+      });
+    })
+    .parse(command);
+});
+
 describe('CLI "comments" command', () => {
-  let csvError;
-  let csvOutput;
+  let err;
+  let output;
 
-  beforeAll(async (done) => {
-    const rawOutput = await new Promise((resolve) => {
-      const stdout = mockStdout();
-      cli
-        .onFinishCommand(() => {
-          const stdOutString = stdout.read();
-          stdout.restore();
-          resolve(stdOutString);
-        })
-        .parse(`comments -c ${global.__MONGO_CONNECTION_STRING__} -s 0`);
-    });
-
-    csvParse(rawOutput, (err, output) => {
-      csvError = err;
-      csvOutput = output;
-      done();
-    });
+  beforeAll(async () => {
+    [err, output] = await getCSVFromCommand(`comments -c ${global.__MONGO_CONNECTION_STRING__} -s 0`);
   });
 
   it('is a valid CSV', () => {
-    expect(csvError).toBe(undefined);
+    expect(err).toBe(undefined);
   });
 
   it('returns correct number of results', () => {
-    expect(csvOutput).toHaveLength(4);
+    expect(output).toHaveLength(4);
   });
 
   it('contains timestamps in column 1', () => {
-    const column = csvOutput.map((row) => row[0]);
+    const column = output.map((row) => row[0]);
     expect(column[0]).toEqual('Datetime');
     expect(column[1]).toMatch(DATETIME_REGEX);
     expect(column[2]).toMatch(DATETIME_REGEX);
@@ -94,7 +100,7 @@ describe('CLI "comments" command', () => {
   });
 
   it('contains URLs in column 2', () => {
-    const column = csvOutput.map((row) => row[1]);
+    const column = output.map((row) => row[1]);
     expect(column).toEqual([
       'URL',
       'https://example.com/1',
@@ -104,7 +110,7 @@ describe('CLI "comments" command', () => {
   });
 
   it('contains satisfaction in column 3', () => {
-    const column = csvOutput.map((row) => row[2]);
+    const column = output.map((row) => row[2]);
     expect(column).toEqual([
       'Satisfied',
       'true',
@@ -114,12 +120,62 @@ describe('CLI "comments" command', () => {
   });
 
   it('contains comments column 4', () => {
-    const column = csvOutput.map((row) => row[3]);
+    const column = output.map((row) => row[3]);
     expect(column).toEqual([
       'Comments',
       'This is comment 1',
       'This is comment 2',
       '',
+    ]);
+  });
+});
+
+describe('CLI "averages" command', () => {
+  let err;
+  let output;
+
+  beforeAll(async () => {
+    [err, output] = await getCSVFromCommand(`averages -c ${global.__MONGO_CONNECTION_STRING__} -s 0`);
+  });
+
+  it('is a valid CSV', () => {
+    expect(err).toBe(undefined);
+  });
+
+  it('returns correct number of results', () => {
+    expect(output).toHaveLength(5);
+  });
+
+  it('contains URLs in column 1', () => {
+    const column = output.map((row) => row[0]);
+    expect(column).toEqual([
+      'URL',
+      'https://example.com/4',
+      'https://example.com/3',
+      'https://example.com/2',
+      'https://example.com/1',
+    ]);
+  });
+
+  it('contains number satisfied in column 2', () => {
+    const column = output.map((row) => row[1]);
+    expect(column).toEqual([
+      'satisfied',
+      '3',
+      '1',
+      '0',
+      '1',
+    ]);
+  });
+
+  it('contains number unsatisfied in column 3', () => {
+    const column = output.map((row) => row[2]);
+    expect(column).toEqual([
+      'unsatisfied',
+      '1',
+      '0',
+      '1',
+      '0',
     ]);
   });
 });
